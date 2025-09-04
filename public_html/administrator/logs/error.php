37dc58f180fc1e2090f263823dbf7b36602db041
Suetrong Samoepakpoom <sentsuetrong@gmail.com>
2025-09-04 13:11:25 +0700

    update .gitignore file

--- public_html/administrator/logs/error.php
+++ public_html/administrator/logs/error.php
@@ -5,3 +5,4 @@
 
 #Fields: datetime	priority clientip	category	message
 2025-09-01T07:58:47+00:00	INFO 172.18.0.1	joomlafailure	คุณต้องใส่รหัสผ่าน
+2025-09-04T04:21:15+00:00	INFO 172.18.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
